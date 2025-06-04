1a109270f88c0e18483df46d92ca624db1f7c89b
Josseth Arroyo <55711477+cheche482010@users.noreply.github.com>
2025-06-04 14:22:08 -0400

    tiempo toast

--- web/src/views/Payment/Payment.js
+++ web/src/views/Payment/Payment.js
@@ -346,6 +346,7 @@ export default {
               description: "Tu pedido ha sido registrado",
             })
 
+            await new Promise(resolve => setTimeout(resolve, 2000))
             // Redirigir a confirmación
             router.push("/user/orders")
           } catch (paymentError) {
